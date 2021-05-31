0c46a44d729de9ecc0921b6b08a19233309eac16
LiuXsss <77252851+LiuXsss@users.noreply.github.com>
2021-05-30 21:32:29 -0700

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repositories;
+
+import org.springframework.stereotype.Repository;
+import java.util.LinkedHashMap;
+
+@Repository
+public class UserRepository extends LinkedHashMap<String, Object> {
+}
