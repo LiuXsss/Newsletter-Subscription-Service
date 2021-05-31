c754a47bc7ed9e123e80ef08e16d0f3e5e24141a
LiuXsss <77252851+LiuXsss@users.noreply.github.com>
2021-05-30 23:10:01 -0700

    Delete demo directory

--- demo/src/main/java/com/example/demo/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.stereotype.Repository;
-import java.util.LinkedHashMap;
-
-@Repository
-public class UserRepository extends LinkedHashMap<String, Object> {
-}
